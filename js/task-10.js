67b7764dc00d63aa91bf19e39e46340e34c7a582
Andrii <sushkoandy1@gmail.com>
2023-02-27 11:13:26 +0200

    Fix 10 tasck

--- js/task-10.js
+++ js/task-10.js
@@ -10,7 +10,7 @@ const btnCreateEl = document.querySelector("[data-create]");
 const btnDestroyEl = document.querySelector("[data-destroy]");
 const divBoxesEl = document.querySelector("#boxes");
 
-btnCreateEl.addEventListener("click", createBoxes(inputValue.value));
+btnCreateEl.addEventListener("click", createBoxes);
 btnDestroyEl.addEventListener("click", () => {
 	divBoxesEl.innerHTML = "";
 });
@@ -18,9 +18,10 @@ btnDestroyEl.addEventListener("click", () => {
 	inputValue.value = "";
 });
 
-function createBoxes(amount) {
+function createBoxes() {
+	const maxLengthValue = inputValue.value;
 	const firstDivSize = 30;
-	for (let i = 0; i < amount; i += 1) {
+	for (let i = 0; i < maxLengthValue; i += 1) {
 		let divSize = firstDivSize + 10 * i;
 		const divEl = `<div style=" width: ${divSize}px; height: ${divSize}px; background-color: ${getRandomHexColor()}"></div>`;
 		divBoxesEl.insertAdjacentHTML("beforeend", divEl);
